96cdcb051da854e74d4880fe1811b3267ab19b93
ethanharvey98 <Ethan.Harvey@tufts.edu>
2023-06-09 22:05:29 -0400

    Renamed files and fixed folds.py

--- encode_images/encode_Chest_X-Ray.py
+++ encode_images/encode_Chest_X-Ray.py
@@ -6,11 +6,11 @@ from utils import *
 from torchvision.utils import save_image
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='encode_chest_xray.py')
+    parser = argparse.ArgumentParser(description='encode_Chest_X-Ray.py')
     parser.add_argument('--directory', type=str, help='Directory where images are saved', required=True)
     args = parser.parse_args()
     model = init_encoder()
-    for subdir, dirs, files in os.walk(os.path.join(args.directory, 'chest_xray/')):
+    for subdir, dirs, files in os.walk(args.directory):
         for file_index, file in enumerate(files):
             if re.search('.jpeg$', file):
                 label = re.split('\/', subdir)[-1]
@@ -19,6 +19,6 @@ if __name__=='__main__':
                 assert split in ['train', 'test'], 'Unexpected split: {}'.format(split)
                 path = os.path.join(subdir, file)
                 encoded_image = encode_image(model, path)
-                makedir_if_not_exist(os.path.join(args.directory, 'encoded_chest_xray/{}/{}/'.format(split, label)))
-                encoded_path = os.path.join(args.directory, 'encoded_chest_xray/{}/{}/{}.pt'.format(split, label, re.split('\.', file)[0]))
+                makedir_if_not_exist(os.path.join(os.path.dirname(args.directory), '/encoded_Chest_X-Ray/{}/{}/'.format(split, label)))
+                encoded_path = os.path.join(os.path.dirname(args.directory), '/encoded_Chest_X-Ray/{}/{}/{}.pt'.format(split, label, re.split('\.', file)[0]))
                 torch.save(encoded_image, encoded_path)
--- src/folds.py
+++ src/folds.py
@@ -10,20 +10,28 @@ def create_folds(df, random_state=42):
     if not hasattr(random_state, 'rand'):
         raise ValueError('Not a valid random number generator')
         
-    rows, columns = df.shape
-    
-    numpy_labels = np.array([np.array(label) for label in df.label.to_list()])
+    # Create DataFrame with the most frequent label for each unique subject_id
+    df.index.name = 'subject_id'
+    grouped = df.groupby('subject_id')['label'].agg(lambda x: x.value_counts().idxmax()).reset_index()
+
+    rows, columns = grouped.shape
+
+    numpy_labels = np.array([np.array(label) for label in grouped.label.to_list()])
     unique_labels, counts = np.unique(numpy_labels, axis=0, return_counts=True)
-    folds_for_each_label = [random_state.choice(10, count) for count in counts]
-    
-    folds = -1*np.ones(rows)
+
+    # Unique folds for each subject_id evenly distributed across labels
+    unique_folds = [random_state.choice(10, count) for count in counts]
+
+    fold_numbers = -1*np.ones(rows)
 
     for unique_label_index, unique_label in enumerate(unique_labels):
-        folds[np.all(numpy_labels == unique_label, axis=1)] = folds_for_each_label[unique_label_index]
-        
-    return folds.astype(int)
+            fold_numbers[np.all(numpy_labels == unique_label, axis=1)] = unique_folds[unique_label_index]
+
+    return df.index.map(dict(zip(grouped['subject_id'].to_numpy(), fold_numbers))).astype(int)
 
 def split_folds(df, train_indices=[0, 1, 2, 3, 4, 5, 6, 7], val_indices=[8], test_indices=[9]):
+    
+    assert 'Fold' in df.columns, 'DataFrame is missing Fold column'
     
     train_df = df[df.Fold.isin(train_indices)].copy()
     val_df = df[df.Fold.isin(val_indices)].copy()
